7720aeaad0617dc51f9936ccbc6286c241e27d1d
Jaime Jacobo <jaimejacobordv@gmail.com>
2021-05-06 09:48:09 +0200

    searchbar finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    animals: [
+      "perro",
+      "gato",
+      "jirafa",
+      "pajaro",
+      "aguila",
+      "avestruz",
+      "hamster",
+    ],
+    comparativeAnimals: [
+      "perro",
+      "gato",
+      "jirafa",
+      "pajaro",
+      "aguila",
+      "avestruz",
+      "hamster",
+    ],
+  };
+
+  filterAnimals(event) {
+    const { value } = event.target;
+    const filteredAnimals = this.state.animals.filter((animal) => {
+      return animal.includes(value);
+    });
+    this.setState({ ...this.state, comparativeAnimals: filteredAnimals });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Mis animales</h1>
+        <input
+          onChange={(event) => this.filterAnimals(event)}
+          type="text"
+          placeholder="Busca a tus animales"
+        />
+        <ul>
+          {this.state.comparativeAnimals.map((animal, index) => {
+            return <li key={index}>{animal}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
